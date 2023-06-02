958efbb780d2787ba11a6cc2a20d3cb017358aea
Matthew Toffey <mjtoffey@gmail.com>
2023-06-02 03:05:31 +0000

    added html, css and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+function updateTime() {
+    const display = document.getElementById('clock');
+    const date = new Date();
+        console.log(display)
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+        
+    display.innerText=`${hour} : ${minutes} : ${seconds}`
+}
 
+function formatTime(time) {
+    if ( time < 10 ) {
+        return `0${time}`
+    }
+    return time;
+}
+
+setInterval(updateTime, 1000);
